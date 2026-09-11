7fb07ba025ed8e86d0f164e24756b3f5f6e30642
chenhang <40999666+Initial-neko@users.noreply.github.com>
2026-09-11 21:01:03 +0800

    P0.15: mount asynchronous conversation routes

--- src/character_memory/server.py
+++ src/character_memory/server.py
@@ -1,6 +1,7 @@
 import os
 
 from character_memory.api import create_api
+from character_memory.async_web import attach_async_routes
 from character_memory.group_web import attach_group_routes
 from character_memory.history_web import attach_history_routes
 
@@ -9,3 +10,4 @@ config_path = os.getenv("CHARACTER_MEMORY_CONFIG", "config.yaml")
 app = create_api(config_path)
 attach_history_routes(app)
 attach_group_routes(app, config_path)
+attach_async_routes(app)

9a0bd94ae89b567cc27eb8939bd18c3ee155db12
chenhang <40999666+Initial-neko@users.noreply.github.com>
2026-09-12 23:49:34 +0800

    Attach visual capability to Character Runtime

--- src/character_memory/server.py
+++ src/character_memory/server.py
@@ -6,6 +6,7 @@ from character_memory.avatar_web import attach_avatar_routes
 from character_memory.group_web import attach_group_routes
 from character_memory.history_web import attach_history_routes
 from character_memory.search_web import attach_search_routes
+from character_memory.visual_web import attach_visual_routes
 from character_memory.wake_web import attach_wake_routes
 
 
@@ -13,6 +14,7 @@ config_path = os.getenv("CHARACTER_MEMORY_CONFIG", "config.yaml")
 app = create_api(config_path)
 attach_history_routes(app)
 attach_avatar_routes(app)
+attach_visual_routes(app)
 attach_group_routes(app, config_path)
 attach_search_routes(app)
 attach_async_routes(app)

bb00a0f1f549536666bc66040442dd3f9ad8b176
chenhang <40999666+Initial-neko@users.noreply.github.com>
2026-09-13 12:47:44 +0800

    Mount visual capture routes after async scheduler

--- src/character_memory/server.py
+++ src/character_memory/server.py
@@ -6,6 +6,7 @@ from character_memory.avatar_web import attach_avatar_routes
 from character_memory.group_web import attach_group_routes
 from character_memory.history_web import attach_history_routes
 from character_memory.search_web import attach_search_routes
+from character_memory.visual_capture_web import attach_visual_capture_routes
 from character_memory.visual_web import attach_visual_routes
 from character_memory.wake_web import attach_wake_routes
 
@@ -18,4 +19,5 @@ attach_visual_routes(app)
 attach_group_routes(app, config_path)
 attach_search_routes(app)
 attach_async_routes(app)
+attach_visual_capture_routes(app)
 attach_wake_routes(app)
